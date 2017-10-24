e186b27ca7b7d0a18d15d4beabd882a6b1bcc731
Gary Thompson <sushiguru@gmail.com>
2017-10-24 16:56:45 +0100

    Edited

--- insertElementsWithMenu.js
+++ insertElementsWithMenu.js
@@ -19,20 +19,16 @@ Draw.loadPlugin(function(ui) {
     ui.actions.addAction('myInsertEllipse', function() {
         var theGraph = ui.editor.graph;
         if(theGraph.isEnabled() && !theGraph.isCellLocked(theGraph.getDefaultParent())){
-          var pos=theGraph.getInsertPoint();
-          var newElement=new mxCell("",
-                    new mxGeometry(pos.x, pos.y, 15, 15),
-                    "ellipse;whiteSpace=wrap;html=1;aspect=fixed;shadow=0;glass=0;comic=0;labelBackgroundColor=none;strokeColor=#82b366;strokeWidth=2;fillColor=#d5e8d4;fontFamily=Helvetica;fontSize=14;fontColor=#000000;align=center;verticalAlign=middle;spacing=0;");
-        
-          newElement.vertex=!0;
-          /*newElement.insertVertex(parent, null, '1', pos.x, pos.y, 15, 15, null);*/
-          theGraph.setSelectionCell(theGraph.addCell(newElement))
-			
+            var pos=theGraph.getInsertPoint();
+            var newElement=new mxCell("",
+                new mxGeometry(pos.x, pos.y, 80, 80),
+                "ellipse;whiteSpace=wrap;html=1;");
 
-				
+            newElement.vertex=!0;
+            theGraph.setSelectionCell(theGraph.addCell(newElement))
         }
     }, null, null, "Ctrl+Shift+Q");
-    
+
     ui.keyHandler.bindAction(81, !0, "myInsertEllipse", !0);
   
     
